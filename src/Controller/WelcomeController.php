316391eb732a768f60cde5de33b762039add4312
Maxence <maxence.charles@ik.me>
2026-09-03 17:56:55 +0200

    bootstrap

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/welcome')]
+    public function index(): Response
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+}
